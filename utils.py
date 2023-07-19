c710a22d0c50c6d429fdc8a68064ab6f3a6fa7d7
Beibin Li <beibin79@gmail.com>
2023-07-19 12:03:27 -0700

    List all files recursively

--- utils.py
+++ utils.py
@@ -1,3 +1,31 @@
+import os
+
+
+def display_files_recursively(folder_path, indent='', 
+                              file_suffixes=[".py", ".cpp", ".cs", ".md", ".txt"]):
+    has_valid_file = False  # to check if the folder has at least one valid file
+    
+    valid_files = [file_name for file_name in os.listdir(folder_path) 
+                   if os.path.isfile(os.path.join(folder_path, file_name))
+                   and file_name.endswith(tuple(file_suffixes))]
+    
+    # Display valid files in the current folder
+    for file_name in valid_files:
+        if not has_valid_file:
+            print(indent + os.path.basename(folder_path) + '/')
+            has_valid_file = True
+        print(indent + '    ' + file_name)
+
+    # Recurse into directories
+    for dir_name in os.listdir(folder_path):
+        dir_path = os.path.join(folder_path, dir_name)
+        if os.path.isdir(dir_path):
+            # Recursively check if sub-directory contains valid files or folders with valid files
+            if display_files_recursively(dir_path, indent + '    ', file_suffixes):
+                has_valid_file = True
+
+    return has_valid_file
+
 def find_all_substr(string, substr):
     start_index = 0
     positions = []
