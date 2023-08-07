bc5b4c11c4f951688a58baa97474408018533ace
zhourunlong <zhourunlongvector@gmail.com>
2023-08-06 21:50:10 -0700

    plot curve support multiple modes

--- draw_curve.py
+++ draw_curve.py
@@ -5,6 +5,12 @@ from termcolor import colored
 import argparse
 import matplotlib.pyplot as plt
 
+color_dict = {
+    "pretrain_dir": "red",
+    "baseline_dir": "blue",
+    "direct_dir": "green"
+}
+
 
 def get_args() -> argparse.Namespace:
     """Parses command line arguments for the inference script.
@@ -13,11 +19,19 @@ def get_args() -> argparse.Namespace:
         argparse.Namespace: Parsed arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dir",
+    parser.add_argument("--pretrain_dir",
                         type=str,
                         default=None,
-                        required=True,
-                        help="Dir to load model")
+                        help="Dir to load model after pretrain")
+    parser.add_argument("--baseline_dir",
+                        type=str,
+                        default=None,
+                        help="Dir to load model after baseline")
+    parser.add_argument(
+        "--direct_dir",
+        type=str,
+        default=None,
+        help="Dir to load model directly trained on the finetune data")
     return parser.parse_args()
 
 
@@ -35,23 +49,31 @@ def load_latest_state(experiment_dir):
     latest_checkpoint = max(glob.glob(
         os.path.join(experiment_dir, "checkpoint-*")),
                             key=os.path.getctime)
-    print(colored(f"Loading model from {latest_checkpoint}", "yellow"))
+    print(colored(f"Loading state from {latest_checkpoint}", "yellow"))
     return json.load(open(latest_checkpoint + "/trainer_state.json", "r"))
 
 
 if __name__ == "__main__":
     args = get_args()
 
-    trainer_state = load_latest_state(args.dir)
-    log_history = trainer_state["log_history"]
+    dir_dict = vars(args)
 
-    x, y = [], []
-    for log in log_history:
-        x.append(log["step"])
-        y.append(log["loss"])
+    for mode, path in dir_dict.items():
+        if path is None:
+            continue
 
-    plt.plot(x, y)
+        trainer_state = load_latest_state(path)
+        log_history = trainer_state["log_history"]
+
+        x, y = [], []
+        for log in log_history:
+            x.append(log["step"])
+            y.append(log["loss"])
+
+        plt.plot(x, y, color_dict[mode], label=mode)
+
     plt.title("Loss Curve")
     plt.xlabel("Step")
     plt.ylabel("Loss")
+    plt.legend()
     plt.savefig("loss_curve.png")
